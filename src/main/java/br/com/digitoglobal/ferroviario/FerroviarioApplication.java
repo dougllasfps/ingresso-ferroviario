5093e7053db7a4a6cd39a7e43e4e36a767ea9d8c
Dougllas Sousa <dougllasfps@gmail.com>
2018-12-18 20:28:36 -0300

    criacao do carrinho e tela de visualizacao do produto.

--- src/main/java/br/com/digitoglobal/ferroviario/FerroviarioApplication.java
+++ src/main/java/br/com/digitoglobal/ferroviario/FerroviarioApplication.java
@@ -2,9 +2,10 @@ package br.com.digitoglobal.ferroviario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FerroviarioApplication {
+public class FerroviarioApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FerroviarioApplication.class, args);
